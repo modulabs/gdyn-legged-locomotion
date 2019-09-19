a0064ec395a5ff3fe6ec30576bfe88c886c50f5e
elton-choi <babchol@gmail.com>
2019-09-19 23:32:14 +0900

    Add stand alone.

--- legged_controllers/src/leg_controller.cpp
+++ legged_controllers/src/leg_controller.cpp
@@ -369,10 +369,10 @@ void LegController::update(const ros::Time& time, const ros::Duration& period)
 	_virtual_spring_damper_controller.getControlOutput(_F_leg);
 
 	// * v.03 - force calculation controller: balance controller by MIT cheetah (_F_leg  -  Eigen::Vector3d)
-	//_balance_controller.setControlInput(p_body_d, p_body_dot_d, R_body_d, w_body_d,
-	//						p_body, p_body_dot, R_body, w_body, _p_leg);
-	//_balance_controller.update();
-	//_balance_controller.getControlOutput(_F_leg_balance);
+	_balance_controller.setControlInput(p_body_d, p_body_dot_d, R_body_d, w_body_d,
+							p_body, p_body_dot, R_body, w_body, _p_leg);
+	_balance_controller.update();
+	_balance_controller.getControlOutput(_F_leg_balance);
 
 	// * v.04 - MPC controller: balance controller by MIT cheetah (_F_leg  -  Eigen::Vector3d)
 	//_mpc_controller.setControlInput(p_body_d, p_body_dot_d, R_body_d, w_body_d,
